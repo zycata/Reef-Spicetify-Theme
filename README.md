c9ae8b36d92e73b9cd0ba38cd34b90b3d7f0ab4a
Zyc <166348388+zycata@users.noreply.github.com>
2026-06-21 15:11:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reef Spicetify Theme
+An ocean inspired theme for spicetify
+
+
+
+
+
+### Credits: 
+- **Inspiration and Reference** — Theme **Spicetify Cat** By Adrien5902  
+  [Spicetify Cat](https://github.com/Adrien5902/SpicetifyCat)
---
+++ theme.js
@@ -0,0 +1,41 @@
+const DEFAULT_THEME = "Crustaceans"
+const GITHUB_URL = "https://github.com/zycata/Reef-Spicetify-Theme/blob/main/assets"
+
+function getTheme() {
+    // If no color_scheme is set it should return "" which evaluates to false in JS
+    return Spicetify?.Config?.color_scheme || DEFAULT_THEME;
+}
+
+function waitForElement(els, func, timeout = 100) {
+    const queries = els.map((el) => document.querySelector(el));
+    if (queries.every((a) => a)) {
+        func(queries);
+    } else if (timeout > 0) {
+        setTimeout(waitForElement, 300, els, func, --timeout);
+    }
+}
+
+function refreshTheme(theme) {
+    // refreshLikedSongsImage(theme)
+
+    waitForElement([".Root__top-container"], ([el]) => {
+        el.style.backgroundImage = `url(${encodeURI(`${GITHUB_URL}/${theme}/background.png?raw=true`)})`;
+    })
+}
+
+function refreshLikedSongsImage(theme) {
+    for (const selector of likedSongsImageSelectors) {
+        for (const img of document.querySelectorAll(selector)) {
+            img.setAttribute("src", `${GITHUB_URL}/${theme}/liked_songs.png?raw=true`)
+            img.removeAttribute("srcset")
+        }
+    }
+}
+
+
+
+
+
+
+
+refreshTheme()
